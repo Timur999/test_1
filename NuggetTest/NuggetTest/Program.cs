8b9853ac89f0d383a8fc4d3383c6ee78c1909b6a
dota2 <dota2@poczta.onet.pl>
2021-03-17 15:03:38 +0100

    added project

---
+++ NuggetTest/NuggetTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UtfUnknown;
+
+namespace TestNugget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../file/test.txt";
+
+            DetectionResult result = CharsetDetector.DetectFromFile(path);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
